42f46dffd30eae4dc930d822654caf2d6db1a0f8
maetzemax <maetze35@>
2023-09-03 23:57:51 +0200

    proper movement controller

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -1,15 +1,19 @@
 using UnityEngine;
 
 public class Movement : MonoBehaviour {
-    private Rigidbody rb;
-    private GameManager gameManager;
+    private CharacterController controller;
+    
 
-    [SerializeField]
-    private float movementSpeed = 10f;
-    public float jumpHeight = 2f;
+    public float aimingSpeed = 2.0f;
 
+
+    private bool groundedPlayer;
     [SerializeField]
-    private float aimingSpeed = 10f;
+    private float playerSpeed = 2.0f;
+
+    private Vector3 playerVelocity;
+    private float jumpHeight = 1.0f;
+    private float gravityValue = -9.81f;
 
     public GameObject followTarget;
 
@@ -18,49 +22,10 @@ public class Movement : MonoBehaviour {
 
     // Start is called before the first frame update
     void Start() {
-        rb = GetComponent<Rigidbody>();
-        gameManager = GameManager.Instance;
-
         Cursor.lockState = CursorLockMode.Locked;
         Cursor.visible = false;
+        controller = gameObject.AddComponent<CharacterController>();
     }
-
-    private bool isMoving {
-        get {
-            return (rgtMovement || lftMovement || fwdMovement || bwdMovement);
-        }
-    }
-
-    private bool fwdMovement {
-        get {
-            return Input.GetKey(KeyCode.W);
-        }
-    }
-
-    private bool bwdMovement {
-        get {
-            return Input.GetKey(KeyCode.S);
-        }
-    }
-
-    private bool rgtMovement {
-        get {
-            return Input.GetKey(KeyCode.D);
-        }
-    }
-
-    private bool lftMovement {
-        get {
-            return Input.GetKey(KeyCode.A);
-        }
-    }
-
-    private bool isGrounded {
-        get {
-            return rb.velocity.y == 0;
-        }
-    }
-
 
     // Update is called once per frame
     void Update() {
@@ -85,45 +50,26 @@ public class Movement : MonoBehaviour {
             angles.x = 40;
         }
 
-        if (isGrounded && Input.GetKeyDown(KeyCode.Space)) {
-            rb.AddForce(Vector3.up * jumpHeight);
-        }
-
         followTarget.transform.localEulerAngles = angles;
-
         nextRotation = Quaternion.Lerp(followTarget.transform.rotation, nextRotation, Time.deltaTime * rotationLerp);
-
-        if (!isMoving) {
-
-            // TODO: Wrap in aiming value true if aiming is implemented
-            // transform.rotation = Quaternion.Euler(0, followTarget.transform.rotation.eulerAngles.y, 0);
-            // followTarget.transform.localEulerAngles = new Vector3(angles.x, 0, 0);
-            return;
-        }
-
-        Vector3 position = new Vector3();
-        var speed = movementSpeed / 10;
-
-        if (!isGrounded) {
-            speed *= 0.75f;
-        }
-
-        if (fwdMovement)
-            position += (transform.forward * speed);
-
-        if (bwdMovement)
-            position -= (transform.forward * speed);
-
-        if (rgtMovement)
-            position += (transform.right * speed);
-
-        if (lftMovement)
-            position -= (transform.right * speed);
-
-
-        transform.position += Vector3.ClampMagnitude(position, 1f);
 
         transform.rotation = Quaternion.Euler(0, followTarget.transform.rotation.eulerAngles.y, 0);
         followTarget.transform.localEulerAngles = new Vector3(angles.x, 0, 0);
+
+        groundedPlayer = controller.isGrounded;
+        if (groundedPlayer && playerVelocity.y < 0) {
+            playerVelocity.y = 0f;
+        }
+
+        controller.Move(transform.forward * Input.GetAxis("Vertical") * Time.deltaTime * playerSpeed);
+        controller.Move(transform.right * Input.GetAxis("Horizontal") * Time.deltaTime * playerSpeed);
+
+        // Changes the height position of the player..
+        if (Input.GetButtonDown("Jump") && groundedPlayer) {
+            playerVelocity.y += Mathf.Sqrt(jumpHeight * -3.0f * gravityValue);
+        }
+
+        playerVelocity.y += gravityValue * Time.deltaTime;
+        controller.Move(playerVelocity * Time.deltaTime);
     }
 }
