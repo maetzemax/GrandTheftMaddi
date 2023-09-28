74b1ec9c97766ee2732b36390226dfb28d493fae
maetzemax <maetze35@>
2023-09-28 21:59:28 +0200

    xp dropable + collectable / UI Changes ingame (XP Bar)

--- Assets/Scripts/Characters/Player.cs
+++ Assets/Scripts/Characters/Player.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using static UnityEditor.Experimental.GraphView.GraphView;
 
 public class Player: CharacterStats {
 
@@ -24,8 +25,17 @@ public class Player: CharacterStats {
 
     [Header("XP")]
     public int xp;
+    public int nextLvlXp = 10;
+    public int currentLevel = 1;
 
     private void Update() {
+        if (xp >= nextLvlXp) {
+            currentLevel += 1;
+            xp = 0;
+            nextLvlXp += Mathf.RoundToInt(nextLvlXp * 1.5f);
+        }
+
+
         if (currentHealth <= 0.0f) {
             Destroy(gameObject);
             GameManager.currentGameState = GameManager.GameState.Menu;
--- Assets/XP.cs
+++ Assets/XP.cs
@@ -3,21 +3,10 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class XP : MonoBehaviour {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
     // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
-    private void OnCollisionEnter(Collision collision) {
-        print(collision.gameObject.name);
-        if (collision.gameObject.CompareTag("Player")) {
+    void Update() {
+        var distanceToPlayer = Vector3.Distance(transform.position, Player.instance.gameObject.transform.position);
+        if (distanceToPlayer < 0.7f) {
             Destroy(gameObject);
             Player.instance.xp += 1;
         }
