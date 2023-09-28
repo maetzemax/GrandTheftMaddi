81a177e5f3da5443b61c138bf1caa842320789c9
maetzemax <maetze35@>
2023-09-28 20:30:47 +0200

    moved setting to main menu scene + menu/ingame music

--- Assets/Scripts/UI/MainMenu.cs
+++ Assets/Scripts/UI/MainMenu.cs
@@ -2,11 +2,33 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour {
+    public Camera cam;
+    public Canvas settings;
+    public Canvas menu;
+
+    bool rotateToSettings = false;
+    bool rotateToMenu = true;
+
+    private void Update() {
+        if (settings.gameObject.active) {
+            Quaternion currentRotation = cam.transform.rotation;
+            Quaternion wantedRotation = Quaternion.Euler(-30, -110, 0);
+            cam.transform.rotation = Quaternion.RotateTowards(currentRotation, wantedRotation, Time.deltaTime * 500f);
+        }
+
+        if (menu.gameObject.active) {
+            Quaternion currentRotation = cam.transform.rotation;
+            Quaternion wantedRotation = Quaternion.Euler(-30, 30, 0);
+            cam.transform.rotation = Quaternion.RotateTowards(currentRotation, wantedRotation, Time.deltaTime * 500f);
+        }
+    }
+
     public void PlayGame() {
         SceneManager.LoadScene(1);
     }
 
     public void OpenSettings() {
-        SceneManager.LoadScene(2);
+        settings.gameObject.active = true;
+        menu.gameObject.active = false;
     }
 }
